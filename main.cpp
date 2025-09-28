a3b6653ee85a63175d7ca9313d5a18996511f064
augfabb <augdogwww@gmail.com>
2025-09-28 03:02:29 +0000

    update

--- main.cpp
+++ main.cpp
@@ -1,13 +1,13 @@
 // ------------- FILE HEADER -------------
-// Author ✅:
+// Author ✅: 
 // Assignment ✅:
 // Date ✅:
-// Citations:
+// Citations: 
 
 
 // ------------- ZYBOOKS SCORES -------------
-// Chapter ✅:
-// Participation ✅:
+// Chapter ✅: 
+// Participation ✅: 
 // Challenge ✅:
 // Labs ✅:
 
@@ -15,63 +15,111 @@
 // ------------- DISCORD POSTS -------------
 // https://discord.com/invite/URYKKf8YHm
 // Count ✅:
-// Links (Optional):
+// Links (Optional): 
 
 
 // ------------- DESIGN DOCUMENT -------------
-// A. INPUT ✅:
+// A. INPUT ✅: 
 // B. OUTPUT ✅:
 // C. CALCULATIONS ✅:
 // D. LOGIC and ALGORITHMS ✅:
-//    (Optional) flow chart link or file name:
+//    (Optional) flow chart link or file name: 
 
 
 // ------------- TESTING -------------
-// PASS ALL GIVEN SAMPLE RUN TESTS ✅:
-// (Optional) Additional tests count:
+// PASS ALL GIVEN SAMPLE RUN TESTS ✅: 
+// (Optional) Additional tests count:   
 
 
 // ------------- CODE -------------
 #include <iostream>
-
 using namespace std;
-
-// Function prototypes (if any)
-
-
 // Main function
-// https://en.cppreference.com/w/cpp/language/main_function.html
 int main(int argc, char* argv[]) {
-    cout << "Hello, World!" << endl;
-    return 0;
+  
+  int employee_id = 0;
+  int hours_worked = 0;
+  int usd_per_hour = 0;
+  int federal_withholding_percent = 0;
+
+  int gross_pay = 0;
+  double federal_tax_witholding_usd = 0.0;
+  double net_pay_usd = 0.0;
+
+  gross_pay = hours_worked * usd_per_hour;
+  double decimal_tax_witholding_usd = federal_withholding_percent/100;
+  double net_pay_usd = gross_pay * decimal_tax_witholding_usd;
+
+  
+
+
+
+
+  
+  
+  
+  
+  return 0;
 }
 
 // Function implementations (if any)
 
 
 // ------------- DESIGN -------------
-/*
+/* 
 Program Name:
 
 Program Description:
 
 Design:
 A. INPUT
-Define the input variables including name data type.
+int employee_id = 0;
+int hours_worked = 0;
+int usd_per_hour = 0;
+int federal_withholding_percent = 0;
 
 B. OUTPUT
-Define the output variables including data types.
+int gross_pay = 0
+double federal_tax_witholding_usd = 0.0;
+double net_pay_usd = 0.0;
 
 C. CALCULATIONS
-Describe calculations used by algorithms in step D.
-List all formulas.
-If there are no calculations needed, state there are no calculations.
+gross_pay = hours_worked * usd_per_hour
+double decimal_tax_witholding_usd = federal_withholding_percent/100
+double net_pay_usd = gross_pay * decimal_tax_witholding_usd
+
+
 
 D. LOGIC and ALGORITHMS
-Design the logic of your program using pseudocode or flowcharts.
-Use conditionals, loops, functions or array constructs.
-List the steps in transforming inputs into outputs.
-https://github.com/Glen-Sasek-PCC-Instructor/2025-06-22/blob/main/Pseudocode-Reference.txt
+MESSAGE "Welcome to my Weekly Payroll program!!"
+
+PROMPT "Enter your employee ID number (numbers only):"
+INPUT   int employee_id
+
+PROMPT "Enter number of hours worked (whole numbers):"
+INPUT   int hours_worked
+
+PROMPT "Enter the hourly rate:"
+INPUT    int usd_per_hour
+
+PROMPT "Enter the federal withholding rate"
+INPUT    federal_withholding_percent
+
+MESSAGE "Your Payroll Summary:"
+
+CALCULATIONS DONE HERE
+
+LABEL "Total Gross Pay: $"
+DATA   gross_pay
+
+LABEL "Federal Tax Withholding: $"
+DATA   federal_tax_witholding_usd
+
+LABEL "Net Pay: $"
+DATA   net_pay_usd
+
+
+
 
 
 SAMPLE RUNS
